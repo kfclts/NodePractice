8c3497e016c28ed3af4ec67a84b2365c118b469f
KFC <kfchang@fstop.com.tw>
2019-05-05 02:19:54 +0800

    Displaying the Cart Items (195)

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,17 +53,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -72,7 +67,7 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then(product => {
-      return req.user.addToCart(product);
+      req.user.addToCart(product);
     })
     .then(result => {
       console.log(result);
