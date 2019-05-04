6d86cfb3fb8dbce3cca6e051790f5cdebc8710d8
KFC <kfchang@fstop.com.tw>
2019-05-05 01:37:56 +0800

    Creating New Users(190)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -20,12 +21,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
+  User.findById('5baa2528563f16379fc8a610')
+    .then(user => {
+      req.user = user;
+       next();
+     })
+     .catch(err => console.log(err));
   next();
 });
 
