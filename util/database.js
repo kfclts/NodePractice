62a2c5d0aa793ba321ea9d390c7fffa4923e722c
KFC <kfchang@fstop.com.tw>
2019-05-04 12:27:46 +0800

    Installing the MongoDB Driver(176-2)

--- util/database.js
+++ util/database.js
@@ -1,9 +1,8 @@
-<reference path="modules/mongodb/index.d.ts" />
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 const mongoConnect = callback => {
-  MongoClient.connect('mongodb+srv://KFC:49002109@cluster0-gzkx8.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
+  MongoClient.connect('mongodb+srv://USER:PASSWORD@cluster0-gzkx8.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
   .then( client => {
     console.log('Connected!')
     callback(client);
