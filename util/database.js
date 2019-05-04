5da57ed7419c10ec14ef98a85d16670348994425
KFC <kfchang@fstop.com.tw>
2019-05-04 13:42:54 +0800

    Finishing the Database Connection(178)

--- util/database.js
+++ util/database.js
@@ -1,15 +1,26 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
   MongoClient.connect('mongodb+srv://USER:PASSWORD@cluster0-gzkx8.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
   .then( client => {
     console.log('Connected!')
-    callback(client);
+    _db = client._db();
+    callback();
   })
   .catch( err => {
     console.log(err);
+    throw err;
   });
 }
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+}
 
-module.exports = mongoConnect;
+module.mongoConnect = mongoConnect;
+module.getDb = getDb;
