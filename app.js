dc47e7e432fcdc9fdb625375ba05f2e34f6b7df8
KFC <kfchang@fstop.com.tw>
2019-05-05 14:51:34 +0800

    Saving Data Through Mongoose

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,14 +20,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5baa2528563f16379fc8a610')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5baa2528563f16379fc8a610')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -36,7 +36,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://USER:PASSWORD@cluster0-gzkx8.gcp.mongodb.net/test?retryWrites=true', { useNewUrlParser: true }
+    'mongodb+srv://USER:PASSWORD@cluster0-gzkx8.gcp.mongodb.net/shop?retryWrites=true', { useNewUrlParser: true }
   )
   .then(result => {
     app.listen(3000);
